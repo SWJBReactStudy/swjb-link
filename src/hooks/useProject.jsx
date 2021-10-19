0329a8d40fcb70065bc39d642a15e4a51b18e5da
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-10-19 11:49:23 +0900

    fix: Project Context 수정

--- src/hooks/useProject.jsx
+++ src/hooks/useProject.jsx
@@ -4,7 +4,13 @@ const Context = createContext(null);
 
 export const ProjectProvider = ({ children }) => {
   const projects = useState({
-    1: [{ title: '헬스타트', desc: '운동을 시작하는 당신에게 어울리는 프로젝트', link: 'http://test.itsw.info/6' }],
+    1: [
+      { title: '헬스타트', desc: '운동을 시작하는 당신에게 어울리는 프로젝트', link: 'http://test.itsw.info/6' },
+      { title: '헬타트', desc: ' 당신에게 어울리는 프로젝트', link: 'http://test.itsw.info/5' },
+      { title: '헬스타트', desc: '운동을 시작하는 당신에게 어울리는 프로젝트', link: 'http://test.itsw.info/6' },
+      { title: '헬스타트', desc: '운동을 시작하는 당신에게 어울리는 프로젝트', link: 'http://test.itsw.info/6' },
+      { title: '헬스타트', desc: '운동을 시작하는 당신에게 어울리는 프로젝트', link: 'http://test.itsw.info/6' },
+    ],
     2: [],
     3: [],
   })[0];
@@ -12,7 +18,7 @@ export const ProjectProvider = ({ children }) => {
   return <Context.Provider value={projects}>{children}</Context.Provider>;
 };
 
-const useProject = ({ grade }) => {
+const useProject = (grade) => {
   const data = useContext(Context);
   return grade ? data[grade] : data;
 };
--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './style/global.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import App from './App';
+import { ProjectProvider } from './hooks/useProject';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ProjectProvider>
+      <App />
+    </ProjectProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
