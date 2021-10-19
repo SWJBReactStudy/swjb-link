c73cfa7d66b199e654ec20c61f5125dd8d80c174
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-10-19 11:35:10 +0900

    feat: 프로젝트 관리할 context 생성

---
+++ src/hooks/useProject.jsx
@@ -0,0 +1,20 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const Context = createContext(null);
+
+export const ProjectProvider = ({ children }) => {
+  const projects = useState({
+    1: [{ title: '헬스타트', desc: '운동을 시작하는 당신에게 어울리는 프로젝트', link: 'http://test.itsw.info/6' }],
+    2: [],
+    3: [],
+  })[0];
+
+  return <Context.Provider value={projects}>{children}</Context.Provider>;
+};
+
+const useProject = ({ grade }) => {
+  const data = useContext(Context);
+  return grade ? data[grade] : data;
+};
+
+export default useProject;
