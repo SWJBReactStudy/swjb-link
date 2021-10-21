4546cfe0d896dcd20afbb40248422c8da0893aa5
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-10-21 15:39:37 +0900

    build: rererebuild

--- src/asset/projectImage.js
+++
@@ -1,9 +0,0 @@
-import One from './one.PNG';
-import Molto from './molto.PNG';
-import Starterdy from './startudy.PNG';
-import Swjb from './swjb.PNG';
-import Health from './healthStart.PNG';
-import Indian from './indian.PNG';
-import Sinamon from './sinamon.PNG';
-
-export { One, Molto, Starterdy, Swjb, Health, Indian, Sinamon };
