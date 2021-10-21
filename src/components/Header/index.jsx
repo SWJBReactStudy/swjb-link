8b5e5dc36d97abcee5abf3c75168f778358f8519
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-10-21 15:35:25 +0900

    fix: 헤더 padding 변경

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,13 +6,13 @@ import Logo from '../../asset/logo.png';
 const Container = styled.div`
   width: 100vw;
   height: 68px;
-  padding: 0 3rem;
+  padding: 1.5rem 3rem;
   background: rgb(20, 20, 20);
   background: linear-gradient(to bottom, rgba(0, 0, 0, 0.7) 10%, rgba(0, 0, 0, 0));
 `;
 
 const Image = styled.img`
-  height: 68px;
+  height: 58px;
   cursor: pointer;
   opacity: 0.7;
   transition: opacity 150ms ease-in-out;
